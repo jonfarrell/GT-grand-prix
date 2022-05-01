5c7f6c71960d2b65e820eab79c4dbbf9fcec668f
jonfarrell <hi@jonfarrell.com>
2022-05-01 13:43:30 +1000

    update prod mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,8 +99,19 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { :host => "https://gtgrandprix.herokuapp.com" }
 
+  config.action_mailer.raise_delivery_errors = false
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    tls:                  true,
+    domain:               'gmail.com',
+    user_name:            'secret@email.com',
+    password:             'secret',
+    authentication:       'plain',
+    enable_starttls_auto: true  
+}
 
   # Inserts middleware to perform automatic connection switching.
   # The `database_selector` hash is used to pass options to the DatabaseSelector
